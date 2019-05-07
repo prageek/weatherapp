cec0ac9e340b9ab9ffc5ef7ec768692403dc232d
Prageeth <prag123@gmail.com>
2019-05-07 14:37:02 -0400

    proper commit

---
+++ weatherapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by Prageeth Kumara on 5/7/19.
+//  Copyright © 2019 Prageeth Kumara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
